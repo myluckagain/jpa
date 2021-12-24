c355380a5e9ee38ba70c85a58e90d966202c68ea
myluckagain <myluckagain@gmail.com>
2021-12-24 13:21:51 +0500

    init 3

--- src/test/java/ru/some/service/UserServiceTest.java
+++ src/test/java/ru/some/service/UserServiceTest.java
@@ -7,6 +7,6 @@ import org.springframework.test.annotation.Commit;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @Commit
-public class Test {
+public class UserServiceTest {
 
 }
